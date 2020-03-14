d491ac6634836d53760b64f563d97d01499bd5f2
Daniel Love <danlove99@hotmail.co.uk>
2020-03-14 11:07:41 +0000

    Update main.py

--- main.py
+++ main.py
@@ -10,9 +10,7 @@ myclient = pymongo.MongoClient("mongodb://localhost:27017/")
 
 
 dblist = myclient.list_database_names()
-'''if "UserInfo" in dblist:
-	pass
-else:'''
+
 mydb = myclient["UserInfo"]
 users = mydb["users"]
 
